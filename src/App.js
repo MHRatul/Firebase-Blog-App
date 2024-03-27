eb621b216dfba44a4ec7a4617b7925baa0ed6f64
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-03-27 15:39:21 +0600

    Project Set-up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./pages/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="App">
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
