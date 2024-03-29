9d90d896d4145af54f8c901c6230df0bce172e57
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-03-29 19:26:31 +0600

    update style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+
 import Home from "./pages/Home";
 import {Routes, Route} from "react-router-dom"
 import { ToastContainer } from "react-toastify";
