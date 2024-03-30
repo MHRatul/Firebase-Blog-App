adcf7dc6f42fed0a729df52759b2b1d12e747d34
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-03-30 12:13:45 +0600

    Update HeaderJs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,117 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import transitions from "bootstrap";
 
-const Header = () => {
+const Header = ({ active, setActive, user, handleLogout }) => {
+  const userId = user?.uid;
   return (
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <div className="container-fluid bg-faded padding-media">
-       <div className="container padding-media">
-         <nav className="navbar navbar-toggleable-md navbar-light">
-          <button className='navbar-toggler mt-3' 
-          type='button'
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          data-bs-parent="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="true"
-          aria-label="Toggle Navigation"
-           > 
-               <span className='fa fa-bars'></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid bg-faded padding-media">
+        <div className="container padding-media">
+          <nav className="navbar navbar-toggleable-md navbar-light">
+            <button
+              className="navbar-toggler mt-3"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              data-bs-parent="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="true"
+              aria-label="Toggle Navigation"
+            >
+              <span className="fa fa-bars"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                  <li
+                    className={`nav-item nav-link ${
+                      active === "home" ? "active" : ""
+                    }`}
+                    onClick={() => setActive("home")}
+                  >
+                    Home
+                  </li>
+                </Link>
+                <Link to="/blogs" style={{ textDecoration: "none" }}>
+                  <li
+                    className={`nav-item nav-link ${
+                      active === "blogs" ? "active" : ""
+                    }`}
+                    onClick={() => setActive("blogs")}
+                  >
+                    Blogs
+                  </li>
+                </Link>
 
-          </button>
-          <div className="collapse navbar-collapse"></div>
-            <ul className='navbar-nav me-auto mb-2 mb-lg-0' id='navbarSupportedContent'>
-               <li className='nav-item nav-link'>Home</li>
-               <li className='nav-item nav-link'>Create</li>
-               <li className='nav-item nav-link'>About</li>
-            </ul>
+                <Link to="/create" style={{ textDecoration: "none" }}>
+                  <li
+                    className={`nav-item nav-link ${
+                      active === "create" ? "active" : ""
+                    }`}
+                    onClick={() => setActive("create")}
+                  >
+                    Create
+                  </li>
+                </Link>
 
-            <div className='row g-3'>
-            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-               <li className='nav-item nav-link'>Login</li>
-            </ul>
+                <Link to="/about" style={{ textDecoration: "none" }}>
+                  <li
+                    className={`nav-item nav-link ${
+                      active === "about" ? "active" : ""
+                    }`}
+                    onClick={() => setActive("about")}
+                  >
+                    About
+                  </li>
+                </Link>
+              </ul>
+              <div className="row g-3">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  {userId ? (
+                    <>
+                      <div className="profile-logo">
+                        <img
+                          src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                          alt="logo"
+                          style={{
+                            width: "30px",
+                            height: "30px",
+                            borderRadius: "50%",
+                            marginTop: "12px",
+                          }}
+                        />
+                      </div>
+                      <p style={{ marginTop: "12px", marginLeft: "5px" }}>
+                        {user?.displayName}
+                      </p>
+                      <li className="nav-item nav-link" onClick={handleLogout}>
+                        Logout
+                      </li>
+                    </>
+                  ) : (
+                    <Link to="/auth" style={{ textDecoration: "none" }}>
+                      <li
+                        className={`nav-item nav-link ${
+                          active === "login" ? "active" : ""
+                        }`}
+                        onClick={() => setActive("login")}
+                      >
+                        Login
+                      </li>
+                    </Link>
+                  )}
+                </ul>
+              </div>
             </div>
-         </nav>
-       </div>
-       </div>
-     </nav>
-  )
-}
+          </nav>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
